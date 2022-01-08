70ad9f91affecf5eed181878db1ce65593b77e6c
Mileidy Melendez <mileidymelendez1@gmail.com>
2022-01-08 18:54:19 -0400

    Cambios en la vista Migrar del perfil administrador

--- resources/views/administrator/database/migrate.blade.php
+++ resources/views/administrator/database/migrate.blade.php
@@ -18,6 +18,24 @@
  <!--  BEGIN CONTENT AREA  -->
  <div id="content" class="main-content mt-6">
     <div class="layout-px-spacing">
+        <div class="mb-0">
+            <div class="row mb-2 mt-3">
+                <div class="col-12 col-md-7 col-lg-7 col-xl-7 mb-0 order-md-0 order-1 mb-2 mt-2">
+                    <nav class="breadcrumb-two align-self-center" aria-label="breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item active"><a href="{{ route('home') }}">Inicio</a></li>
+                            <li class="breadcrumb-item" ><a href="javascript:void(0);">Migrate</a></li>
+                        </ol>
+                    </nav>
+                </div>
+                <div class="col-md-4 col-lg-4 col-xl-4 justify-content-end mb-0 d-flex mt-4 ml-auto">
+                    <img src="assets/img/logo-sicano4.png" class="img-fluid mb-0 d-none d-md-block" alt="header-image" style="width-sm: 23%; width-md: 35%; width: 40%">
+                </div>
+            </div>
+        </div>
+
+ <div id="content" class="main-content mt-6">
+    <div class="layout-px-spacing">
         <div class="mt-2 mb-5">
             <div class="container mb-0">
                 <div class="row mb-2 justify-content-center">
