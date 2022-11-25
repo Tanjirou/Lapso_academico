89365f223275bd268e92968c18d5dcf21dd0a762
tanjirou <lugotorrealba@gmail.com>
2022-11-24 22:17:33 -0400

    Arreglando error de datetime con mysql

--- database/seeders/UserTypesSeeder.php
+++ database/seeders/UserTypesSeeder.php
@@ -17,21 +17,15 @@ class UserTypesSeeder extends Seeder
         DB::table('user_types')->insert([
             'description' => 'Administrador',
             'status' => 'A',
-            'created_at' => '14-11-2021',
-            'updated_at' => '14-11-2021'
         ]);
         DB::table('user_types')->insert([
             'description' => 'Profesor',
             'status' => 'A',
-            'created_at' => '14-11-2021',
-            'updated_at' => '14-11-2021'
         ]);
 
         DB::table('user_types')->insert([
             'description' => 'Estudiante',
             'status' => 'A',
-            'created_at' => '14-11-2021',
-            'updated_at' => '14-11-2021'
         ]);
     }
 }
