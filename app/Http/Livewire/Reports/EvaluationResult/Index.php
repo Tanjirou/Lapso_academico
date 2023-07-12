66f5281d53648898fd97b6d4366be522ee1c41cb
Mileidy <mileidymelendez1@gmmail.com>
2023-07-12 19:21:29 -0400

    Actualizacion perfil jefe dptoo reporte

--- app/Http/Livewire/Reports/EvaluationResult/Index.php
+++ app/Http/Livewire/Reports/EvaluationResult/Index.php
@@ -28,7 +28,7 @@ class Index extends Component
         $this->department = Department::where('id',$this->teacher->ndepartament)->first();
         $this->academic_lapse = AcademicLapse::where('status','A')->first();
         $this->lapse = $this->academic_lapse->id;
-        $this->detailSection = new DetailSection();
+        $this->detailSections = null;
         $this->department_sections = DepartmentSection::where('departmentid',$this->department->id)->get();
         $this->department_sections = DepartmentSection::join('structure_sections','department_sections.id','=','structure_sections.department_sectionid')
         ->join('subjects','structure_sections.subjectid','=','subjects.id')
@@ -92,26 +92,30 @@ class Index extends Component
         $optionsub = $this->optionsub;
         $selectedDepartmentSection = $this->selectedDepartmentSection;
         $selectedSubject = $this->selectedSubject;
-        $this->detailSections = DetailSection::join('sections','detail_sections.sectionid','=','sections.id')
+        $detailSections = DetailSection::join('sections','detail_sections.sectionid','=','sections.id')
         ->join('subjects','sections.subjectid','=','subjects.id')
         ->join('department_sections','subjects.departmentsectionid','=','department_sections.id')
         ->join('departments','department_sections.departmentid','=','departments.id')
         ->where('sections.status','=','F')
         ->where('detail_sections.status','=','F')
-        ->orWhere(function ($query) use ($optionds, $selectedDepartmentSection) {
-            if ($optionds && isset($optionds) && $optionds != 'option1' && $selectedDepartmentSection){
-                $query->where('department_sections.id','=',$selectedDepartmentSection);
-            }
-        })
-        ->orWhere(function ($query) use ($optionsub, $selectedSubject) {
-            if ($optionsub && isset($optionsub) && $optionsub != 'option3' && isset($selectedSubject) && !is_null($selectedSubject)){
-                $query->where('subjects.id','=',$selectedSubject);
-            }
-        })
 
-        ->select('department_sections.description as department_section','subjects.name as subject','sections.section_number')
-        ->groupBy('department_sections.description','subjects.name','sections.section_number')
-        ->simplePaginate(10);
+
+
+        // ->select('department_sections.description as department_section','subjects.code as code','subjects.name as subject','sections.section_number as section_number');
+        ->selectRaw("department_sections.description as department_section, subjects.code as code, subjects.name as subject, sections.section_number as section_number,
+        COUNT(CASE WHEN detail_sections.qualification = 'aprobado' THEN 1 END) as aprobados,
+        COUNT(CASE WHEN detail_sections.qualification = 'reprobado' THEN 1 END) as reprobados")
+        ->groupBy('department_sections.description', 'subjects.code','subjects.name','sections.section_number');
+        // ->simplePaginate(10);
+
+        if($this->optionds && $optionds != 'option1'){
+           $detailSections->where('department_sections.id','=',$this->selectedDepartmentSection);
+        }
+        if($this->optionsub && $optionsub != 'option3'){
+            $detailSections->where('subjects.id','=',$this->selectedSubject);
+         }
+        $this->detailSections = $detailSections->get();
+
     }
     public function render()
     {
@@ -121,3 +125,14 @@ class Index extends Component
 // ->select('department_sections.description as department_section','subjects.name as subject','sections.section_number', COUNT(CASE WHEN detail.sections.qualification == 'Aprobado' THEN 1 END) as studaprob, COUNT(CASE WHEN detail.sections.qualification == 'Reprobado' THEN 1 END) as studrep)
 
 // $nombre_de_columna=$results[0]->nombre_de_columna
+
+ // ->orWhere(function ($query) use ($optionds, $selectedDepartmentSection) {
+        //     if ($optionds && isset($optionds) && $optionds != 'option1' && $selectedDepartmentSection){
+        //         $query->where('department_sections.id','=',$selectedDepartmentSection);
+        //     }
+        // })
+        // ->orWhere(function ($query) use ($optionsub, $selectedSubject) {
+        //     if ($optionsub && isset($optionsub) && $optionsub != 'option3' && isset($selectedSubject) && !is_null($selectedSubject)){
+        //         $query->where('subjects.id','=',$selectedSubject);
+        //     }
+        // })
