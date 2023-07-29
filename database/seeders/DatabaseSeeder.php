039679839909c089aa98b6f8db87c8d6f8d2d47c
tanjirou <lugotorrealba@gmail.com>
2023-07-29 17:38:34 -0400

    Eliminando los seeders de prueba

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,16 +15,11 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
         $this->call(UserTypesSeeder::class);
-        $this->call(AcademicLapseSeeder::class);
         $this->call(DepartmentSeeder::class);
         $this->call(AcademicCurriculaSeeder::class);
         $this->call(UserSeeder::class);
         $this->call(DepartmentSectionsSeeder::class);
         $this->call(SubjectsSeeder::class);
-        $this->call(StructureSectionSeeder::class);
-        $this->call(SectionSeeder::class);
-        $this->call(StudentsSeeder::class);
-        $this->call(DetailSectionSeeder::class);
         $this->call(MentionSeeder::class);
 
 
